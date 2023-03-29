08ea9a041c3ab1974c3e6d09358175778d253234
yuliiaishchenko <romashka.ukr@gmail.com>
2023-03-29 14:27:05 +0300

    fix

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name';
 
-export function fetchCounties (name) {
-fetch(`${BASE_URL}/{name}?fields=name,capital,population,flags,languages`).then(
+export function fetchCounties(name) {
+return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`).then(
 
     response => {
         if (!response.ok)
